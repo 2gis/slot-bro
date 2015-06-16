6ef096d27b9e4784a08602469450188773e49b0a
Дементьев Евгений Владимирович <e.dementev@2gis.ru>
2015-06-16 10:34:19 +0600

    EXTENSION-201 add script loader for popup

---
+++ extension-builder/kango/src/patches/common/kango-ui/kango_api.js
@@ -0,0 +1,43 @@
+(function(win) {
+    var fireReady = win.KangoAPI._fireReady;
+    var require = chrome.extension.getBackgroundPage()._kangoLoader.require;
+
+    win.KangoAPI._fireReady = function() {
+        fireReady.call(win.KangoAPI);
+
+        if (win.KangoAPI.getBackgroundPage()) {
+            var scripts = require('kango/extension_info').popup_scripts;
+
+            bindScript(scripts);
+        }
+    };
+
+    function bindScript(scripts, i) {
+        var scriptUrl;
+        var scriptNode;
+        var extensionFileUrl;
+        i = i || 0;
+        if (i >= scripts.length) return;
+
+        scriptUrl = scripts[i];
+        scriptNode = win.document.createElement("script");
+        scriptNode.setAttribute("type", "text/javascript");
+        extensionFileUrl = kango.io.getExtensionFileUrl(scriptUrl);
+        scriptNode.setAttribute("src", extensionFileUrl);
+        scriptNode.onerror = function() {
+            console.error("Unable to load " + scriptUrl)
+        };
+        if (scriptNode.onreadystatechange) {
+            scriptNode.onreadystatechange = function() {
+                if (scriptNode.readyState == "complete") {
+                    bindScript(scripts, i + 1);
+                }
+            }
+        } else {
+            scriptNode.onload = function() {
+                bindScript(scripts, i + 1);
+            };
+        }
+        win.document.body.appendChild(scriptNode);
+    }
+})(window);
